c2bc7c1e72c59b295f1b699f34ce72b438f52e62
Jonas Pleyer <jonas.pleyer@fdm.uni-freiburg.de>
2024-10-25 05:33:00 +0200

    add initial docs template

---
+++ docs/conf.py
@@ -0,0 +1,32 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'cellular_raza_template_pyo3'
+copyright = '2024, Jonas Pleyer'
+author = 'Jonas Pleyer'
+release = '0.1.0'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    'myst_parser',
+    'sphinx.ext.autodoc',
+    'sphinxcontrib.bibtex',
+]
+
+templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+bibtex_bibfiles = ['references.bib']
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'sphinx_rtd_theme'
+html_static_path = ['_static']
---
+++ docs/index.rst
@@ -0,0 +1,17 @@
+.. cellular_raza_template_pyo3 documentation master file, created by
+   sphinx-quickstart on Fri Oct 25 04:39:26 2024.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+cellular_raza_template_pyo3 documentation
+=========================================
+
+Add your content using ``reStructuredText`` syntax. See the
+`reStructuredText <https://www.sphinx-doc.org/en/master/usage/restructuredtext/index.html>`_
+documentation for details.
+
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
